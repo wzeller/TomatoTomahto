8f209e1964e74d038f2fc012f743baeb3b7524c6
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-14 15:42:24 -0700

    add session_tolen column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130814184542) do
+ActiveRecord::Schema.define(:version => 20130814223245) do
 
   create_table "users", :force => true do |t|
     t.string   "username",        :null => false
     t.string   "password_digest", :null => false
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "session_token",   :null => false
   end
 
+  add_index "users", ["session_token"], :name => "index_users_on_session_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
