a74302f698f31f7c69502c2391ab59bd77569904
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-14 13:51:23 -0700

    add validation on password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessible :username, :password
+  attr_reader :password
+
+  validates :username, :presence => true
+  validates :password_digest, :presence => true, :message => "Password can't be blank"
+  validates :password, :length => { :minimum => 6 }, :allow_blank => true
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
