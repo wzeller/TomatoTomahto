180e65ad0d79e4226d300bfea4c86975ec9d4e77
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-14 16:28:12 -0700

    add User#reset_session_token!

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,17 @@ class User < ActiveRecord::Base
     SecureRandom::urlsafe_base64(16)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  def is_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password?(password)
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
   end
 
   private
