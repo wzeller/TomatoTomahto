e9ac4a310ffe780392cb12dfb24014d651352051
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-24 16:36:14 -0700

    add reasonable filters to sessions/users controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  before_filter :require_no_current_user!, :only => [:create, :new]
+  before_filter :require_current_user!, :only => [:destroy]
+
   def create
     user = User.find_by_credentials(
       params[:user][:username],
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :require_current_user!, :except => [:create, :new]
+  before_filter :require_current_user!, :only => [:show]
+  before_filter :require_no_current_user!, :only => [:create, :new]
 
   def create
     @user = User.new(params[:user])
